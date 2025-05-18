959896c1331b8edf262b26e3ad2110864e706e09
archive <ryanantongiovanni0@gmail.com>
2025-05-17 20:19:40 -0700

    Photon was fixed and tested

--- src/MeowEngine/Bot/PhotonBot.js
+++ src/MeowEngine/Bot/PhotonBot.js
@@ -1,12 +1,11 @@
 import WebSocket from "ws";
 import ProtocolReader from "../Photon/protocol_reader/ProtocolReader";
-import { PacketType, OperationCode, InternalOperationCode, EventCode, ParameterCode } from "../Photon/protocol_reader/constants"
-import { PacketBuilder } from "../../Photon/Utils/PacketBuilder";
+import { PacketType, OperationCode, InternalOperationCode, EventCode, ParameterCode } from "../Photon/protocol_reader/constants";
+import { PacketBuilder, createPacket } from "../../Photon/Utils/PacketBuilder"; // Updated import
 const runningLobbies = [];
 
 export class PhotonBot {
     constructor(config = {}) {
-        // Configuration with defaults
         this.roomName = config.roomName || "";
         this.username = config.username || `PC-${Math.random().toString(36).substr(2, 9)}`;
         this.lobbySocket = null;
@@ -19,12 +18,10 @@ export class PhotonBot {
         this.previousActorList = [];
         this.userFinderResult = { username: "", lobby: "", found: false };
 
-        // Initialize WebSocket connection
         this.lobbySocket = new WebSocket(`wss://game-ca-1.blayzegames.com:2053/?libversion=4.1.6.10&sid=30&app=`, "GpBinaryV16");
         this.setupLobbySocket();
     }
 
-    // Parse command-line arguments
     static parseArgs() {
         const args = {};
         process.argv.slice(2).forEach(arg => {
@@ -37,7 +34,6 @@ export class PhotonBot {
         return args;
     }
 
-    // Generate UUID
     generateUUID() {
         return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, c => {
             const r = Math.random() * 16 | 0;
@@ -46,20 +42,17 @@ export class PhotonBot {
         });
     }
 
-    // Log method
     botLog(...args) {
         console.log(`[MeowEngine Bot ${this.username}]:`, ...args);
     }
 
-    // Get random lobby
     getRandomLobby() {
         if (runningLobbies.length === 0) return null;
         const index = Math.floor(Math.random() * runningLobbies.length);
         return runningLobbies[index];
     }
 
-    // Fetch authentication code
-    async getAuthCode() {
+    async getAuthCode(username, hashedPassword) {
         let auth = "";
         const response = await fetch("https://server.blayzegames.com/OnlineAccountSystem/get_multiplayer_auth_code.php?requiredForMobile=769220037", {
             headers: {
@@ -76,14 +69,13 @@ export class PhotonBot {
                 Referer: "https://bullet-force-multiplayer.game-files.crazygames.com/",
                 "Referrer-Policy": "strict-origin-when-cross-origin"
             },
-            body: "password=437B864DD79157AAEC7F9E4687CD2ECE5A4318F5D7DBDB4ABE819C3F9CE22E51B650A6578D2BF3ED6FFA451382E16DEBC7247AB768A19ACBEC4C9E0786C3EE4E&username=PC-543654y54454&username=PC-543654y54454&password=437B864DD79157AAEC7F9E4687CD2ECE5A4318F5D7DBDB4ABE819C3F9CE22E51B650A6578D2BF3ED6FFA451382E16DEBC7247AB768A19ACBEC4C9E0786C3EE4E",
+            body: `password=${hashedPassword}&username=${username}&username=${username}&password=${hashedPassword}`,
             method: "POST"
         });
         auth = await response.text();
         return auth;
     }
 
-    // Time-related methods
     _tickCount() {
         return Date.now() - this._startTime.getTime();
     }
@@ -92,143 +84,131 @@ export class PhotonBot {
         return this._tickCount() + this._serverTickOffset;
     }
 
-    // Send ping packet
     sendPing(socket) {
-        const pingRequest = PacketBuilder.createRequest(1)
-            .addParam(1, PacketBuilder.types.integer(this._tickCount()));
-        const pingBuffer = pingRequest.toBuffer();
-        socket.send(pingBuffer);
+        const pingRequest = createPacket()
+            .operation(1)
+            .withInteger(1, this._tickCount())
+            .build();
+        socket.send(pingRequest);
     }
 
-    // Ping loop
     pingLoop(socket) {
         setInterval(() => {
-            const pingRequest = PacketBuilder.createRequest(InternalOperationCode.Ping)
-                .addParam(1, PacketBuilder.types.integer(1000));
-            const pingBuffer = pingRequest.toBuffer();
-            socket.send(pingBuffer);
+            const pingRequest = createPacket()
+                .internalOperation(InternalOperationCode.Ping)
+                .withInteger(1, 1000)
+                .build();
+            socket.send(pingRequest);
         }, 1000);
     }
 
-    // Send authentication parameters
     sendAuthParams() {
-        const packet = PacketBuilder.createRequest(OperationCode.Authenticate)
-            .addParam(220, PacketBuilder.types.string("1.104.5_HC_1.105"))
-            .addParam(224, PacketBuilder.types.string("8c2cad3e-2e3f-4941-9044-b390ff2c4956"))
-            .addParam(210, PacketBuilder.types.string("eu/*"))
-            .addParam(225, PacketBuilder.types.string(this.generateUUID()));
-        const bufferData = packet.toBuffer();
-        this.lobbySocket.send(bufferData);
+        const packet = createPacket()
+            .operation(OperationCode.Authenticate)
+            .withString(220, "1.104.5_HC_1.105")
+            .withString(224, "8c2cad3e-2e3f-4941-9044-b390ff2c4956")
+            .withString(210, "eu/*")
+            .withString(225, this.generateUUID())
+            .build();
+        this.lobbySocket.send(packet);
     }
 
-    // Send game authentication
     sendGameAuth(token) {
         this.botLog("sendGameAuth -> ", token);
-        const packet = PacketBuilder.createRequest(OperationCode.Authenticate)
-            .addParam(221, PacketBuilder.types.string(token));
-        const bufferData = packet.toBuffer();
-        this.gameSocket.send(bufferData);
+        const packet = createPacket()
+            .operation(OperationCode.Authenticate)
+            .withString(221, token)
+            .build();
+        this.gameSocket.send(packet);
     }
 
-    // Send join lobby request
     sendJoinLobby(socket) {
-        const packet = PacketBuilder.createRequest(OperationCode.JoinLobby);
-        const bufferData = packet.toBuffer();
-        socket.send(bufferData);
+        const packet = createPacket()
+            .operation(OperationCode.JoinLobby)
+            .build();
+        socket.send(packet);
     }
 
-    // Join a room
     joinRoom(roomName) {
-        const packet = PacketBuilder.createRequest(OperationCode.JoinGame)
-            .addParam(255, PacketBuilder.types.string(roomName));
-        const bufferData = packet.toBuffer();
-        this.lobbySocket.send(bufferData);
+        const packet = createPacket()
+            .operation(OperationCode.JoinGame)
+            .withString(255, roomName)
+            .build();
+        this.lobbySocket.send(packet);
     }
 
-    // Kick player with reason
     kickPlayerWithReason(targetActorId, reason) {
-        const packet = PacketBuilder.createRequest(OperationCode.RaiseEvent)
-            .addParam(ParameterCode.Code, PacketBuilder.types.byte(200))
-            .addParam(ParameterCode.Cache, PacketBuilder.types.byte(0))
-            .addParam(ParameterCode.ActorList, PacketBuilder.types.intArray([targetActorId]))
-            .addParam(ParameterCode.Data, PacketBuilder.types.hashTable([
-                [PacketBuilder.types.byte(4), PacketBuilder.types.objectArray([
-                    PacketBuilder.types.string(reason)
-                ])],
-                [PacketBuilder.types.byte(5), PacketBuilder.types.byte(91)],
-            ]));
-        const bufferData = packet.toBuffer();
-        this.gameSocket.send(bufferData);
+        const packet = createPacket()
+            .event(200)
+            .withInteger(ParameterCode.Code, 200)
+            .withInteger(ParameterCode.Cache, 0)
+            .withIntArray(ParameterCode.ActorList, [targetActorId])
+            .withHashtable(ParameterCode.Data, {
+                4: [reason],
+                5: 91
+            })
+            .build();
+        this.gameSocket.send(packet);
     }
 
-    // Send announcement
     sendAnnouncement(text, duration) {
-        const packet = PacketBuilder.createRequest(OperationCode.RaiseEvent)
-            .addParam(ParameterCode.Code, PacketBuilder.types.byte(200))
-            .addParam(ParameterCode.Cache, PacketBuilder.types.byte(4))
-            .addParam(ParameterCode.Data, PacketBuilder.types.hashTable([
-                [PacketBuilder.types.byte(0), PacketBuilder.types.integer(1001)],
-                [PacketBuilder.types.byte(4), PacketBuilder.types.objectArray([
-                    PacketBuilder.types.string(text),
-                    PacketBuilder.types.float(duration),
-                ])],
-                [PacketBuilder.types.byte(5), PacketBuilder.types.byte(61)],
-            ]));
-        const bufferData = packet.toBuffer();
-        this.gameSocket.send(bufferData);
+        const packet = createPacket()
+            .event(200)
+            .withInteger(ParameterCode.Code, 200)
+            .withInteger(ParameterCode.Cache, 4)
+            .withHashtable(ParameterCode.Data, {
+                0: 1001,
+                4: [text, duration],
+                5: 61
+            })
+            .build();
+        this.gameSocket.send(packet);
     }
 
-    // Send authentication token
     sendAuthToken(token) {
-        const packet = PacketBuilder.createRequest(OperationCode.RaiseEvent)
-            .addParam(ParameterCode.Code, PacketBuilder.types.byte(200))
-            .addParam(ParameterCode.Cache, PacketBuilder.types.byte(0))
-            .addParam(ParameterCode.Data, PacketBuilder.types.hashTable([
-                [PacketBuilder.types.byte(4), PacketBuilder.types.objectArray([
-                    PacketBuilder.types.string(token)
-                ])],
-                [PacketBuilder.types.byte(5), PacketBuilder.types.byte(88)],
-            ]));
-        const bufferData = packet.toBuffer();
-        this.gameSocket.send(bufferData);
+        const packet = createPacket()
+            .event(200)
+            .withInteger(ParameterCode.Code, 200)
+            .withInteger(ParameterCode.Cache, 0)
+            .withHashtable(ParameterCode.Data, {
+                4: [token],
+                5: 88
+            })
+            .build();
+        this.gameSocket.send(packet);
     }
 
-    // Send join room with properties
     sendJoinRoomWithProperties(roomName) {
-        const packet = PacketBuilder.createRequest(226);
-        packet.addParam(255, PacketBuilder.types.string(roomName));
         const perksArray = new Uint8Array(8);
         perksArray.set([1, 9, 14, 2, 22]);
-        const hashtable249 = PacketBuilder.types.hashTable([
-            [PacketBuilder.types.string("platform"), PacketBuilder.types.string("WebGLPlayer")],
-            [PacketBuilder.types.string("teamNumber"), PacketBuilder.types.byte(0)],
-            [PacketBuilder.types.string("rank"), PacketBuilder.types.byte(27)],
-            [PacketBuilder.types.string("killstreak"), PacketBuilder.types.byte(15)],
-            [PacketBuilder.types.string("characterCamo"), PacketBuilder.types.byte(0)],
-            [PacketBuilder.types.string("bulletTracerColor"), PacketBuilder.types.byte(1)],
-            [PacketBuilder.types.string("glovesCamo"), PacketBuilder.types.byte(16)],
-            [PacketBuilder.types.string("unlockedWeapons"), PacketBuilder.types.array(0x69, [
-                PacketBuilder.types.integer(0),
-                PacketBuilder.types.integer(0),
-                PacketBuilder.types.integer(0)
-            ])],
-            [PacketBuilder.types.string("current_kills_in_killstreak"), PacketBuilder.types.integer(0)],
-            [PacketBuilder.types.string("kd"), PacketBuilder.types.float(8.511835098266602)],
-            [PacketBuilder.types.string("perks"), PacketBuilder.types.byteArray(perksArray)],
-            [PacketBuilder.types.string("current_vehicle_view_id"), PacketBuilder.types.integer(4294967295)],
-            [PacketBuilder.types.string("up_to_date_version"), PacketBuilder.types.string("1.104.5_HC")],
-            [PacketBuilder.types.string("throwable_type"), PacketBuilder.types.integer(12)],
-            [PacketBuilder.types.string("throwable_amount"), PacketBuilder.types.integer(3)],
-            [PacketBuilder.types.string("nextCreateRoomPass"), PacketBuilder.types.string("")],
-            [PacketBuilder.types.byte(255), PacketBuilder.types.string(this.username)]
-        ]);
-        packet.addParam(249, hashtable249);
-        packet.addParam(250, PacketBuilder.types.boolean(true));
-        const bufferData = packet.toBuffer();
-        this.gameSocket.send(bufferData);
+        
+        const packet = createPacket()
+            .operation(226)
+            .withString(255, roomName)
+            .withHashtable(249, {
+                platform: "WebGLPlayer",
+                teamNumber: 0,
+                rank: 27,
+                killstreak: 15,
+                characterCamo: 0,
+                bulletTracerColor: 1,
+                glovesCamo: 16,
+                unlockedWeapons: [0, 0, 0],
+                current_kills_in_killstreak: 0,
+                kd: 8.511835098266602,
+                perks: Buffer.from(perksArray),
+                current_vehicle_view_id: 4294967295,
+                up_to_date_version: "1.104.5_HC",
+                throwable_type: 12,
+                throwable_amount: 3,
+                nextCreateRoomPass: "",
+                255: this.username
+            })
+            .withBoolean(250, true)
+            .build();
+        this.gameSocket.send(packet);
     }
 
-    // Handle player leave
     handleLeave(packet, playerListJson) {
         const currentActorList = packet.params[252].data.map(entry => entry.value);
         const leftActorNr = this.previousActorList.find(nr => !currentActorList.includes(nr));
@@ -245,19 +225,16 @@ export class PhotonBot {
         this.previousActorList = currentActorList;
     }
 
-    // Filter rooms with ID
     filterRoomsWithIdOnly(roomKeys) {
         return roomKeys.filter(key => /\(#\d{4,6}\)/.test(key));
     }
 
-    // Clean username
     cleanUsername(rawName) {
         let cleaned = rawName.replace(/<color=#[A-Fa-f0-9]{6}>/g, '').replace(/<\/color>/g, '');
         cleaned = cleaned.replace(/^\[[^\]]+\]/g, '');
         return cleaned.trim();
     }
 
-    // Convert base64 to Uint8Array
     base64toUint8Array(base64) {
         const binaryString = atob(base64);
         const length = binaryString.length;
@@ -268,38 +245,34 @@ export class PhotonBot {
         return bytes;
     }
 
-    // Unknown packet
     idkWhatPacketThisIs() {
         this.gameSocket.send(this.base64toUint8Array("8wL8AAP7aAABYv9zAA5bXVBDLU5leHRUb1lvdf5pAAAAI/pvAQ==").buffer);
     }
 
-    // Instantiate actor packet
     sendInstantiateActorPacket() {
-        const packet = PacketBuilder.createRequest(253)
-            .addParam(244, PacketBuilder.types.byte(202))
-            .addParam(247, PacketBuilder.types.byte(6))
-            .addParam(252, PacketBuilder.types.array(0x69, [
-                PacketBuilder.types.integer(35)
-            ]));
-        const bufferData = packet.toBuffer();
-        this.gameSocket.send(bufferData);
+        const packet = createPacket()
+            .operation(253)
+            .withInteger(244, 202)
+            .withInteger(247, 6)
+            .withIntArray(252, [35])
+            .build();
+        this.gameSocket.send(packet);
     }
 
-    // Player body packet
     sendPlayerBodyPacket() {
-        const packet = PacketBuilder.createRequest(253)
-            .addParam(244, PacketBuilder.types.byte(202))
-            .addParam(245, PacketBuilder.types.hashTable([
-                [PacketBuilder.types.byte(0), PacketBuilder.types.string("PlayerBody")],
-                [PacketBuilder.types.byte(6), PacketBuilder.types.integer(103197933)],
-                [PacketBuilder.types.byte(7), PacketBuilder.types.integer(1001)]
-            ]))
-            .addParam(247, PacketBuilder.types.byte(4));
-        const bufferData = packet.toBuffer();
-        this.gameSocket.send(bufferData);
+        const packet = createPacket()
+            .operation(253)
+            .withInteger(244, 202)
+            .withHashtable(245, {
+                0: "PlayerBody",
+                6: 103197933,
+                7: 1001
+            })
+            .withInteger(247, 4)
+            .build();
+        this.gameSocket.send(packet);
     }
 
-    // Setup lobby socket event listeners
     setupLobbySocket() {
         this.lobbySocket.onopen = () => {
             this.botLog("Connected to LobbyServer!");
@@ -347,7 +320,6 @@ export class PhotonBot {
         };
     }
 
-    // Setup game socket event listeners
     setupGameSocket() {
         this.gameSocket.onclose = (event) => {
             console.log(`Connection closed for ${this.username}: `, event.code, event.reason);
@@ -414,7 +386,6 @@ export class PhotonBot {
         };
     }
 
-    // Start the bot
     start() {
         setTimeout(() => {
             this.joinRoom(this.roomName);
@@ -423,11 +394,3 @@ export class PhotonBot {
 }
 
 export default PhotonBot;
-
-// Example usage: Create multiple bots
-// const args = BulletForceBot.parseArgs();
-// const bot1 = new BulletForceBot({ roomName: args.roomName || "DefaultRoom1", username: "Bot1" });
-// const bot2 = new BulletForceBot({ roomName: args.roomName || "DefaultRoom2", username: "Bot2" });
-
-// bot1.start();
-// bot2.start();
